f6bf1cc8e3486d1ab2941f8f5545ff33d06993ff
Luis <luislucas@asu.edu>
2024-11-03 14:20:50 -0700

    Added fetch terms function
    Fetch terms gets the terms and their value from the ASU Class Search API.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let body = reqwest::get("https://www.rust-lang.org")
-        .await?
-        .text()
-        .await?;
-
-    println!("{}", body);
+    let terms_resp = fetch_terms().await?;
+    println!("{terms_resp:#?}");
 
     Ok(())
+}
+
+async fn fetch_terms() -> Result<serde_json::Value, reqwest::Error> {
+    let client = reqwest::Client::new();
+    client
+        .get(
+            "https://eadvs-cscc-catalog-api.apps.asu.edu/catalog-microservices/api/v1/search/terms",
+        )
+        .header(
+            "User-Agent",
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/124.0",
+        )
+        .header("Accept", "*/*")
+        .header("Accept-Encoding", "gzip, deflate, br")
+        .header("Referer", "https://catalog.apps.asu.edu/")
+        .header("authorization", "Bearer null")
+        .header("Origin", "https://catalog.apps.asu.edu/")
+        .header("DNT", "1")
+        .send()
+        .await?
+        .json()
+        .await
 }
