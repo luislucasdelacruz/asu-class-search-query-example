9bb27868b110ff2a5b3a179eeed1746341ded674
Luis <luislucas@asu.edu>
2024-11-02 23:07:54 -0700

    Added required crates and sample code
    Just the start of the example. Used to get the project started.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), reqwest::Error> {
+    let body = reqwest::get("https://www.rust-lang.org")
+        .await?
+        .text()
+        .await?;
+
+    println!("{}", body);
+
+    Ok(())
 }
